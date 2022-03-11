c10dc74bd1a407fe436e16e3cabdd587339dbab2
Michael Pacleb <michael.pacleb@gmail.com>
2022-03-11 18:53:53 -0500

    Completed 09_palindromes practice exercise.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,8 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const origArr = [...str.toLowerCase()].filter(char => (/[a-zA-Z]/).test(char))
+    const newArr = origArr.slice().reverse();
+
+    return origArr.every((value, idx) => value === newArr[idx])
 
 };
 
