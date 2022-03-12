72de883443a6de1dea216afcc8892c6acaf74a82
Michael Pacleb <michael.pacleb@gmail.com>
2022-03-11 19:29:27 -0500

    Completed 10_fibonacci practice exercise.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let prevNum = 1;
+    let currNum = 1;
+    let temp;
+    const arr = [];
+    
+    if (num < 0) {
+        return 'OOPS';
+    }
 
+    for(let i = 0; i < num; i++) {
+        if (i === 0 || i === 1) {
+            arr.push(1);
+        } else {
+            arr.push(prevNum + currNum);
+            temp = prevNum + currNum;
+            prevNum = currNum;
+            currNum = temp;
+        }
+    }
+
+    return arr[arr.length - 1];
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
