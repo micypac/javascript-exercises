50125f87257fb73c00c06e66e10840ecfa11a322
Michael Pacleb <michael.pacleb@gmail.com>
2022-03-11 22:13:00 -0500

    Completed 12_findTheOldest practice exercise.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    
+    const currentYear = (new Date()).getFullYear();
+    let winner = people[0];
+    
+    const oldest = people.reduce((winner, person) => {
+        const winnerAge = winner.yearOfDeath ? winner.yearOfDeath - winner.yearOfBirth : currentYear - winner.yearOfBirth; 
+        const age = person.yearOfDeath ? person.yearOfDeath - person.yearOfBirth : currentYear - person.yearOfBirth; 
+        if (age > winnerAge) {
+            return person;
+        } else {
+            return winner;
+        }
+    })
 
+    return oldest;
+    
 };
 
 // Do not edit below this line
